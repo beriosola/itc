98a52d4ab63a7a48d680c02a3b630f9e8290e032
beriosola <beriosola@innovathink.com.ph>
2016-06-29 17:14:38 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,156 @@
+<?php 
+
+get_header(); 
+
+?>
+
+</head>
+
+<body>
+<div class="main-wrapper">
+	<!-- Header -->    
+  <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand logo" href="index.html">
+      	<img src="images/logo.png" />
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right navigator">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="who-we-are.html">Who We Are</a></li>
+          <li><a href="products-and-services.html">Products and Services</a></li>
+          <li><a href="contact-us.html">Contact Us</a></li>
+          <li><a href="careers.html" class="careers">Careers</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    
+    
+    <div class="slideshow owl-carousel">
+ 
+			<div class="item">
+				<img src="images/slide1.jpg"  alt="Slide background"/>
+                <div class="slide-content">
+                	<h2>QUALITY</h2>
+                    <p>We believe that when we do it right for the first time it comes up with strong processes and compliance.</p>
+                </div>				
+			</div>
+            <div class="item">
+				<img src="images/slide2.jpg" alt="Slide background"/>
+                <div class="slide-content">
+                	<h2>CUSTOMER SERVICE</h2>
+                    <p>Our commitment doesn't end in just delivering, it continues with services and support.</p>
+                </div>		
+			</div>         
+            <div class="item">
+				<img src="images/slide3.jpg" alt="Slide background"/>
+                <div class="slide-content">
+                	<h2>USER EXPERIENCE</h2>
+                    <p>Customer satisfaction is our priority and in delivering products and services.</p>
+                </div>
+			</div>
+                        
+	   </div>
+    <!-- Slideshow End -->
+    
+    <div class="main-content top-content">
+    	<div class="container text-center">
+        		<h2 class="font-raleway">"We take great pride in our methodology that provides a systematic approach"</h2>
+        </div>
+    </div>
+    
+    <!-- Content -->
+    <div class="content">
+    	<div class="container">
+            <div class="row clearfix consulting">
+            		<div class="col-sm-4 pull-right">
+                    	<img src="images/consulting.jpg" class="img-circle img-responsive" />
+                    </div>
+                    <div class="col-sm-8 pull-left">                    	
+                        <h2>Consulting</h2>
+                        <p>We initially review, assess and recommend viable solutions to organizations that need to optimize and improve their infrastructure, processes and business applications to maintain competitiveness on their business areas.</p>
+                        <a href="products-and-services.html#consulting" class="btn btn-primary btn-consulting">Learn More</a>
+                    </div>
+            </div>
+            <div class="row integration">
+            		<div class="col-sm-4">
+                    	<img src="images/integration.jpg" class="img-circle img-responsive" />
+                    </div>
+                    <div class="col-sm-8">
+                    	<h2>Integration</h2>
+                        <p>Our technology experts takes your need deeply on an overall view & execute a much suitable capability for a smarter operation.</p>    
+                        <a href="products-and-services.html#integration" class="btn btn-primary btn-consulting">Learn More</a>                	
+                    </div>
+            </div>
+            <div class="row clearfix professional-service">
+            		<div class="col-sm-4 pull-right">
+                    	<img src="images/professional-service.jpg" class="img-circle img-responsive" />                    	
+                    </div>
+                    <div class="col-sm-8 pull-left">
+                    	<h2>Professional Services</h2>
+                        <p>Our core asset is our people. Our professionals' on their respective areas with proven track records always deliver in a timely manner along with tangible business values.</p>
+                        <a href="products-and-services.html#professional-service" class="btn btn-primary btn-consulting">Learn More</a>
+                    </div>
+            </div>            
+            <div class="row service-management">
+            		<div class="col-sm-4">
+                    	<img src="images/service-management.jpg" class="img-circle img-responsive" />
+                    </div>
+                    <div class="col-sm-8">                    	
+                        <h2>Service Management</h2>
+                        <p>Our broad experiences on process based standards in service desk operations with capabilities across all Tiers for an end-to-end ITIL-based solutions will maximize the benefits for customers in looking after customer service and end-user experiences.</p>
+                        <a href="products-and-services.html#service-management" class="btn btn-primary btn-consulting">Learn More</a>
+                    </div>
+            </div>
+            
+        </div>
+    </div>
+    <!-- Content End -->
+    
+    <!-- Lower Section -->
+    <div class="bottom-section about-us" style="background-image:url(images/about.jpg);">
+    	<div class="container">
+        	<h2>About Us</h2>
+           	<p><strong>InnovaThink</strong> is an information technology enabled services provider who helps organizations focus on their core businesses by introducing cost effectiveness on innovative solutions, processes & methodologies. The strengths of our delivery are its wide life-cycle coverage and its capability to combine it selectively with other standards.<p>
+            <p>We work as your partner with your business and IT to deliver and manage to what it takes using measured and proven standards and practices.</p>
+        </div>
+    </div>
+    <!-- Lower Section End -->
+    
+    <!-- Footer -->
+    <div class="footer">
+    	<div class="container">
+        	<div class="row iso">
+            	<div class="col-sm-12">
+                	<img src="images/iso.png" style="display:inline-block;" />
+                    <span>ISO 9001:2008 Certified</span>
+                </div>
+            </div>
+            <div class="row copyright">
+            	<div class="col-sm-12">
+                	<p>&copy; Copyright 2015 InnovaThink Corporation. All rights reserved.<br />Privacy Policy</p>
+                </div>
+            </div>
+        	
+        </div>
+    </div>
+    <!-- Footer End -->
+    
+</div>
+
+
+
+<?php
+
+get_footer();
+
+?>
+
